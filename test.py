a41436e0ed1d2ee9beb069f8dd20f3d7a98fb094
akmalmaulan123-source <akmalmaulan123@gmail.com>
2026-09-24 14:05:14 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print ("HelloWorld")
+import pandas as pd
+import matplotlib.pyplot as pyplot
+import seaborn as sns
+
+df = pd.read_csv("latihan1.csv")
+print(df.head())
+print(df.shape)
+print(df.columns)
+print(df.info())
